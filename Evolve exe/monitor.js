e1dd25c12598c1a702bf0d8524f13e45b83a0d21
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2024-05-03 14:02:36 -0300

    Inicio script dos calculos

---
+++ Evolve exe/monitor.js
@@ -0,0 +1,26 @@
+const q1uninter = document.querySelector("#q1uninter");
+const q1udemy = document.querySelector("#q1uninter");
+const q1livros = document.querySelector("#q1uninter");
+let totalhoras;
+
+const q2uninter = document.querySelector("#q1uninter");
+const q2udemy = document.querySelector("#q1uninter");
+const q2livros = document.querySelector("#q1uninter");
+let totaldias;
+
+/*const checkUninter = document.querySelector("#checkUninter");
+const checkUdemy = document.querySelector("#checkUdemy");
+const checkLivros = document.querySelector("#checkLivros");*/
+
+const operacao = document.querySelector("#operacao");
+
+executar.addeventlistener("click",calcular);
+
+function calcular() {
+    
+if (operacao === "Somar");
+    if (checkUninter.checked) vlhorauninter = q1uninter.value
+else (operacao === "Media")
+
+
+}
