15a642fd16436614983961f33a28d22cbd8afe4b
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2024-05-03 21:32:04 -0300

    Backup script feito

--- Evolve exe/monitor.js
+++ Evolve exe/monitor.js
@@ -1,26 +1,24 @@
 const q1uninter = document.querySelector("#q1uninter");
-const q1udemy = document.querySelector("#q1uninter");
-const q1livros = document.querySelector("#q1uninter");
-let totalhoras;
+const q1udemy = document.querySelector("#q1udemy");
+const q1livros = document.querySelector("#q1livros");
 
-const q2uninter = document.querySelector("#q1uninter");
-const q2udemy = document.querySelector("#q1uninter");
-const q2livros = document.querySelector("#q1uninter");
-let totaldias;
-
-/*const checkUninter = document.querySelector("#checkUninter");
-const checkUdemy = document.querySelector("#checkUdemy");
-const checkLivros = document.querySelector("#checkLivros");*/
+const q2uninter = document.querySelector("#q2uninter");
+const q2udemy = document.querySelector("#q2udemy");
+const q2livros = document.querySelector("#q2livros");
 
 const operacao = document.querySelector("#operacao");
+const executar = document.querySelector("#executar");
+let resposta = document.querySelector("#resposta");
 
-executar.addeventlistener("click",calcular);
+executar.addEventListener("click", calchoras);
 
-function calcular() {
-    
-if (operacao === "Somar");
-    if (checkUninter.checked) vlhorauninter = q1uninter.value
-else (operacao === "Media")
-
-
+function calchoras() {
+    const vlq1uninter = parseInt(q1uninter.value);
+    const vlq1udemy = parseInt(q1udemy.value);
+    const vlq1livros = parseInt(q1livros.value);
+    const selecao = operacao.value;
+if (selecao === "somar")
+        resposta.innerHTML = vlq1uninter + vlq1udemy + " horas totais";
+if (selecao === "media")
+        resposta.innerHTML = vlq1uninter + vlq1livros;
 }
