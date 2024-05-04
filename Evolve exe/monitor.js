703e30a383b990ae9c7045f2beecfceac0779800
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2024-05-03 22:06:37 -0300

    Término script
    Site agora realiza o calculo de soma ou media quando seleciona as células. Falta adicionar a outra função e mais verificações de celulas preenchidas para execução ou não do código

--- Evolve exe/monitor.js
+++ Evolve exe/monitor.js
@@ -16,9 +16,23 @@ function calchoras() {
     const vlq1uninter = parseInt(q1uninter.value);
     const vlq1udemy = parseInt(q1udemy.value);
     const vlq1livros = parseInt(q1livros.value);
+    let vlcompararuninter = document.getElementById("compararuninter");
+    let vlcompararudemy = document.getElementById("compararudemy");
+    let vlcompararlivros = document.getElementById("compararlivros");
     const selecao = operacao.value;
-if (selecao === "somar")
-        resposta.innerHTML = vlq1uninter + vlq1udemy + " horas totais";
-if (selecao === "media")
-        resposta.innerHTML = vlq1uninter + vlq1livros;
+    if (selecao === "somar")
+        if (vlcompararuninter.checked == true && vlcompararudemy.checked == true)
+                resposta.innerHTML = vlq1uninter + vlq1udemy + " horas totais";
+        else if (vlcompararuninter.checked == true && vlcompararlivros.checked == true)
+                resposta.innerHTML = vlq1uninter + vlq1livros + " horas totais";
+        else (vlcompararudemy.checked == true && vlcompararlivros.checked == true)
+            resposta.innerHTML = vlq1udemy + vlq1livros + " horas totais";
+
+    if (selecao === "media")
+        if (vlcompararuninter.checked == true && vlcompararudemy.checked == true)
+                resposta.innerHTML = (vlq1uninter + vlq1udemy)/2 + " horas de media";
+        else if (vlcompararuninter.checked == true && vlcompararlivros.checked == true)
+                resposta.innerHTML = (vlq1uninter + vlq1livros)/2 + " horas de media";
+        else (vlcompararudemy.checked == true && vlcompararlivros.checked == true)
+            resposta.innerHTML = (vlq1udemy + vlq1livros)/2 + " horas de media";
 }
