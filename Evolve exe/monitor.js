953d9158dfdc4dca8d3223d0bf4ca261cad6d4a5
Andrey Gabriel <andreygabriel.scnd@gmail.com>
2024-05-05 15:12:35 -0300

    Backup correção perguntas
    Script da resposta caso nenhuma comparação seja ativa foi finalizada com sucesso

--- Evolve exe/monitor.js
+++ Evolve exe/monitor.js
@@ -16,18 +16,42 @@ function calchrs() {
         const vlq1udemy = parseInt(q1udemy.value);
         const vlq1livros = parseInt(q1livros.value);
         const selecao = operacao.value;
-        let respostafinal
-    if (selecao === "somar")
-        if (cpuninter.checked == true && cpudemy.checked == true)
-            respostafinal = vlq1uninter + vlq1udemy;
-        else if (cpuninter.checked == true && cplivros.checked == true)
-            respostafinal = vlq1uninter + vlq1livros;
-        else if (cpudemy.checked == true && cplivros.checked == true)
-            respostafinal = vlq1udemy + vlq1livros;
-        return (resposta.innerHTML = respostafinal + " horas totais");      
+        let rescalchrs
+        let textores
+        let nenhumamarc
+    if ((cpuninter.checked == false && cpudemy.checked == false && cplivros.checked == false)
+        ||(cpuninter.checked == false && cpudemy.checked == false)
+        || (cpuninter.checked == false && cplivros.checked == false)
+        || (cpudemy.checked == false && cplivros.checked == false))
+        nenhumamarc = 1;
+    else if ((cpuninter.checked == true && cpudemy.checked == true && cplivros.checked == true)
+        ||(cpuninter.checked == true && cpudemy.checked == true)
+        || (cpuninter.checked == true && cplivros.checked == true)
+        || (cpudemy.checked == true && cplivros.checked == true))
+        nenhumamarc = 0;
+
+
+    
+    if (nenhumamarc === 1)
+        textores = "";
+    if (nenhumamarc === 0)
+        textores = " horas totais";
+    
+    if (nenhumamarc = 1)
+            rescalchrs = "Selecione 2 ou 3 para comparar"
+        if (selecao === "somar")
+            if (cpuninter.checked == true && cpudemy.checked == true && cplivros.checked == true)
+                rescalchrs = vlq1uninter + vlq1udemy + vlq1livros;
+            else if (cpuninter.checked == true && cpudemy.checked == true)
+                rescalchrs = vlq1uninter + vlq1udemy;
+            else if (cpuninter.checked == true && cplivros.checked == true)
+                rescalchrs = vlq1uninter + vlq1livros;
+            else if (cpudemy.checked == true && cplivros.checked == true)
+                rescalchrs = vlq1udemy + vlq1livros;
+            return (resposta.innerHTML = rescalchrs + textores);      
+        ;
     ;
-    if (selecao === "media")
-        if (cpuninter.checked == true)
-                resposta.innerHTML = (vlq1uninter + vlq1udemy)/2 + " horas totais";;
+        
+    
 }
 
